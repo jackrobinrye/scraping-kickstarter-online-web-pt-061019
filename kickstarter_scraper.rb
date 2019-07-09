51892a6f348546cbac52db2de63e0f67bb432fed
Jack Atherton <jackrrye@gmail.com>
2019-07-09 23:28:47 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,7 +1,7 @@
 require "nokogiri"
 require "pry"
 
-class KickstarterScraper 
+class KickstarterScraper
   def create_project_hash
     html = File.read('fixtures/kickstarter.html')
     kickstarter = Nokogiri::HTML(html)
@@ -21,5 +21,5 @@ class KickstarterScraper
     # return the projects hash
     projects
   end
-  
-end 
+
+end
